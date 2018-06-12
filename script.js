be1e7ebc23ea164aab43824ee83a590a2d78d38b
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-06-12 12:45:23 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var elementButtonClass = document.getElemetsByClassName("button");
+var ilosc_Elementow = elemButtonClass.length;
+var textContent ="";
+for (var i = 0; i <= ilosc_Elementow i++){
+	textContent = elemeButtonClass[i];
+	console.log(textContent.innerText);
