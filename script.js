7610ef12482801d519812dcc2e56aef705f3f6be
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-08-29 10:03:06 +0200

    fix bugged

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var elementButtonClass = document.getElementsByClassName("button");
 var ilosc_Elementow = elementButtonClass.length;
 var textContent ="";
-for (var i = 0; i <= ilosc_Elementow; i++){
+for (var i = 0; i< ilosc_Elementow; i++){
 	textContent = elementButtonClass[i];
 	console.log(textContent.innerText);
 }
